5ddfbccef3b39e8c71376022658e4749d4aa3a07
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-16 15:47:40 +0200

    end 2-snack

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -3,6 +3,11 @@ const people = [
   { name: 'Giulia', age: 24 },
   { name: 'Marco', age: 67 }
 ];
+const peopleName = people.map((person) => {
+  return person.name
+})
+console.log(peopleName);
+
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
