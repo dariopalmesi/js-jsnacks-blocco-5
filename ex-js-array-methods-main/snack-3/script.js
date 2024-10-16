52bb0698ec9f6cc8bffe3077ae1cb75aea511c68
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-16 15:52:17 +0200

    end 3-snack

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,10 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const newNumbers = numbers.map((numb) => {
+    return numb + 1 
+})
+console.log(newNumbers);
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
